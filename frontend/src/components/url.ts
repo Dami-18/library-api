420f6170cb01c9931d851282b31490fada6da868
Dami-18 <damiyagondha@gmail.com>
2025-03-04 09:15:23 +0530

    add api url and template

---
+++ frontend/src/components/url.ts
@@ -0,0 +1,5 @@
+// export const BACKEND_URL = import.meta.env.PROD
+//     ? "https://gyfe-api.metakgp.org"
+//     : "http://127.0.0.1:5000";
+
+export const BACKEND_URL = "http://localhost:8080/lib-api";
